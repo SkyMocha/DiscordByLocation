f8c7626447387556b5aaf0a9b1f01e4e1de9d0c9
AntarcticRuler <34521078+AntarcticRuler@users.noreply.github.com>
2020-06-11 12:03:27 -0400

    Login System
    - Login System using MongoDB and Discord O2Auth implemented

--- index.js
+++ index.js
@@ -8,11 +8,18 @@ const port = 3000
 var MongoClient = require('mongodb').MongoClient;
 const config = require(path.join(__dirname + '/config.json'));
 var url = config.mongo_url;
-var servers;
+var servers, users;
+
+var DiscordStrategy = require('passport-discord').Strategy;
+var passport = require('passport');
+app.use(passport.initialize());
+
+const DiscordOauth2 = require("discord-oauth2");
+const oauth = new DiscordOauth2();
 
 var countyList = JSON.parse(fs.readFileSync(path.join(__dirname + '/gz_2010_us_050_00_20m.json'))).features;
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
 // Connects to the Mongo Database
@@ -20,8 +27,7 @@ MongoClient.connect(url, { useUnifiedTopology: true }, function(err, db) {
   if (err) throw err;
   var dbo = db.db("DiscByLocation");
   servers = dbo.collection("Servers");
-
-  console.log (servers);
+  users = dbo.collection("Users");
 
   // Listens on port 3000
   app.listen(port, () => console.log(`Discord By Location running at http://localhost:${port}`))
@@ -133,5 +139,106 @@ app.post ('/api/counties', function (req, res) {
   console.log (`${counties.length} COUNTIES LOADED`);
 
   res.json (counties);
+
+}); 
+
+
+// DISCORD AUTHENTICATION
+
+var DiscordStrategy = require('passport-discord').Strategy;
+
+var rand = function() { return Math.random().toString(36).substr(2); };
+
+passport.use(new DiscordStrategy(
+  {
+    clientID: '705101854371217418',
+    clientSecret: config.o2auth_secret,
+    callbackURL: `/auth/callback`,
+    scope: ['identify', 'email', 'guilds'],
+  },
+  function(accessToken, refreshToken, profile, cb) {
+  
+    return cb( false, { token: accessToken } );
+
+  })
+);
+
+passport.serializeUser(function(user, done) {
+  // console.log (user);
+  done(null, user);
+});
+passport.deserializeUser(function(user, done) {
+  // console.log (user);
+  done(null, user);
+});
+
+// Authenticates them
+app.get('/auth', passport.authenticate('discord'));
+
+app.get(`/auth/callback`, passport.authenticate('discord', {
+  failureRedirect: `https://www.skymocha.net`
+}), function(req, res) {
+
+  let random_token = rand() + rand(); // Creates a user-specific random token
+
+  let user = req.user; // The user with the data for the user as well as the user ID
+
+  console.log (req.user);
+
+  let mongo_user = {
+    accessToken: user.token,
+    random_token: [random_token]
+  }
+
+  addOrUpdateUser(mongo_user)
+
+  res.redirect(`/?token=${random_token}`) // Successful auth with the random_token
+  res.end();
+});
+
+// Looks to see if there is a user, if there is it updates it, if not it adds the new random_token
+function addOrUpdateUser (mongo_user) {
+  users.find({ accessToken: mongo_user.accessToken }).toArray(function(err, result) {
+    if (err) throw err;
+
+    // NOTHING FOUND; INSERT NEW USER
+    if (result.length == 0) {
+
+      users.insertOne(mongo_user, function(err, result) {
+        if (err) throw err;
+      });
+
+    }
+
+    // HAS ALREADY LOGGED IN BEFORE (CLEARED CACHE)
+    else {
+
+      result[0].random_token.push(mongo_user.random_token[0]);
+
+      let updatedUser = {
+        accessToken: mongo_user.accessToken,
+        random_token: result[0].random_token
+      }
+      users.replaceOne ({accessToken: updatedUser.accessToken}, updatedUser, function(err) {
+        if (err) throw err;
+      });
+
+    }
+    
+  });
+}
+
+// Gets the user based on the random token
+app.post ('/api/user', function (req, res) {
+
+  console.log (req.body);
+  let random_token = req.body.random_token;
+  console.log (random_token);
+
+  // Finds the mongoDB access token for Discord and returns the Discord User
+  users.find ( { $random_token: random_token } ).toArray(function(err, result) { 
+    console.log (result);
+    oauth.getUser(result[0].accessToken).then( token => res.json(token) );
+  });
 
 }); 
